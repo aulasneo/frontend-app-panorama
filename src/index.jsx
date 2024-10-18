a16ac0a709fcc163cbc88e7ce8a7c1619fb009d9
jciasenza <jciasenza@aulasneo.com>
2024-10-18 16:23:04 -0300

    fix favicom

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,11 @@ import 'core-js/stable';
 import 'regenerator-runtime';
 
 import {
-  APP_INIT_ERROR, APP_READY, subscribe, initialize,
+  APP_INIT_ERROR, APP_READY, subscribe, initialize, getConfig,
 } from '@edx/frontend-platform';
 import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
 import ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet';
 
 import Header from '@edx/frontend-component-header';
 import Footer from '@edx/frontend-component-footer';
@@ -17,6 +18,9 @@ import './index.scss';
 subscribe(APP_READY, () => {
   ReactDOM.render(
     <AppProvider>
+      <Helmet>
+        <link rel="shortcut icon" href={getConfig().FAVICON_URL} type="image/x-icon" />
+      </Helmet>
       <Header />
       <AppPanorama />
       <Footer />
